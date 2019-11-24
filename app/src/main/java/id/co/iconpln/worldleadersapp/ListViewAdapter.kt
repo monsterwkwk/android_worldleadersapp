99f1cf8b66cd032999c9d2b251e81e37fb7e0b97
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-11-24 13:23:42 +0700

    [rahmatkurniawan811] Leaders Adapter - Implement method interface

--- app/src/main/java/id/co/iconpln/worldleadersapp/ListViewAdapter.kt
+++ app/src/main/java/id/co/iconpln/worldleadersapp/ListViewAdapter.kt
@@ -1,23 +1,24 @@
 package id.co.iconpln.worldleadersapp
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 
-class ListViewAdapter:BaseAdapter() {
+class ListViewAdapter(val context: Context, val listWorldLeaders: ArrayList<WorldLeaders>):BaseAdapter() {
     override fun getView(index: Int, view: View?, viewGroup: ViewGroup?): View {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun getItem(index: Int): Any {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return listWorldLeaders[index]
     }
 
     override fun getItemId(index: Int): Long {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return index.toLong()
     }
 
     override fun getCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return listWorldLeaders.size
     }
 }
