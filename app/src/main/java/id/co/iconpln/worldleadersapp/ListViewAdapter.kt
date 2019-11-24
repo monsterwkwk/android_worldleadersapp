06cdb810c52b4b05b976d3fbc0466cfadd520fb1
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-11-24 13:19:58 +0700

    [rahmatkurniawan811] Leaders Adapter - add base adapter

---
+++ app/src/main/java/id/co/iconpln/worldleadersapp/ListViewAdapter.kt
@@ -0,0 +1,23 @@
+package id.co.iconpln.worldleadersapp
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+
+class ListViewAdapter:BaseAdapter() {
+    override fun getView(index: Int, view: View?, viewGroup: ViewGroup?): View {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItem(index: Int): Any {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemId(index: Int): Long {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
