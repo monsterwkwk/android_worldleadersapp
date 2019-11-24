9f1a88109fe2d1d8b5d8440998a382b659d02fd9
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-11-24 13:38:38 +0700

    [rahmatkurniawan811] Main - add list adapter

--- app/src/main/java/id/co/iconpln/worldleadersapp/MainActivity.kt
+++ app/src/main/java/id/co/iconpln/worldleadersapp/MainActivity.kt
@@ -1,12 +1,25 @@
 package id.co.iconpln.worldleadersapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val list: ArrayList<WorldLeaders> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadListBaseAdapter(this)
     }
+
+    fun loadListBaseAdapter(context: Context) {
+        list.addAll(WorldLeadersData.listDataWorldLeaders)
+
+        val baseAdapter = ListViewAdapter(context, list)
+        lvListLeader.adapter = baseAdapter
+    }
+
 }
