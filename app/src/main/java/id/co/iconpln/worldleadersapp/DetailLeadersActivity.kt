fcbe49632d2c369dc7f44f776cdb00aeac9b6172
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-11-29 03:07:04 +0700

    [rahmatkurniawan811] DetailLeader - bind view

--- app/src/main/java/id/co/iconpln/worldleadersapp/DetailLeadersActivity.kt
+++ app/src/main/java/id/co/iconpln/worldleadersapp/DetailLeadersActivity.kt
@@ -2,6 +2,9 @@ package id.co.iconpln.worldleadersapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import kotlinx.android.synthetic.main.activity_detail_leaders.*
 
 class DetailLeadersActivity : AppCompatActivity() {
 
@@ -14,5 +17,21 @@ class DetailLeadersActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_leaders)
+
+        displayLeaderDetail()
+    }
+
+    private fun displayLeaderDetail() {
+        tvTitleDetail.text = intent.getStringExtra(EXTRA_NAME)
+        tvDescDetail.text = intent.getStringExtra(EXTRA_DESC)
+        Glide.with(this)
+            .load(intent.getStringExtra(EXTRA_IMAGE_URL))
+            .apply(
+                RequestOptions()
+                    .centerCrop()
+                    .placeholder(R.drawable.ic_launcher_foreground)
+                    .error(R.drawable.ic_launcher_foreground)
+            )
+            .into(ivPictDetail)
     }
 }
