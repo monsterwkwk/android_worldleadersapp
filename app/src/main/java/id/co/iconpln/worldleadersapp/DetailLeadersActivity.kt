b0373ab7763b20e2886cff688270e0c08ab1fcdc
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-11-29 03:01:21 +0700

    [rahmatkurniawan811] DetailLeader - add companion object

--- app/src/main/java/id/co/iconpln/worldleadersapp/DetailLeadersActivity.kt
+++ app/src/main/java/id/co/iconpln/worldleadersapp/DetailLeadersActivity.kt
@@ -5,6 +5,12 @@ import android.os.Bundle
 
 class DetailLeadersActivity : AppCompatActivity() {
 
+    companion object {
+        const val EXTRA_NAME = "extra_name"
+        const val EXTRA_DESC = "extra_desc"
+        const val EXTRA_IMAGE_URL = "extra_image_url"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_leaders)
